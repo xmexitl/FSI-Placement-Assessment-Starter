879875442b7d8706b0a41323cb3112ce48813997
xmexitl <47793884+xmexitl@users.noreply.github.com>
2021-11-29 19:55:35 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,5 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
-
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Xochi" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0      // Gingerbread
@@ -12,12 +9,112 @@ let sugar = 0   // Sugar Sprinkle
 // Code to update name display 
 document.getElementById('credit').textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Gingerbread + button was clicked!')
+let menus = document.querySelectorAll(".menu")
+let totalQty = document.querySelector("#qty-total")
+const buttons = document.querySelectorAll(".button-container")
 
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+
+
+//GINGERBREAD
+let plusGingerBtn = document.querySelector("#add-gb")//button
+let minusGingerBtn = document.querySelector("#minus-gb") //button
+let gingerQty = document.querySelector("#qty-gb") //location
+gingerBtn = parseInt(buttons.textContent)
+
+plusGingerBtn.addEventListener('click', (e) => {
+    gingerPosCount = parseInt(gingerQty.textContent);
+    gingerPosCount = gingerPosCount + 1;
+    gingerQty.textContent = gingerPosCount;
+
+    totalQtyCount = parseInt(totalQty.textContent);
+    totalQtyCount = totalQtyCount + 1;
+    totalQty.textContent = totalQtyCount;
+    
+});
+
+minusGingerBtn.addEventListener('click', (e) => {
+    gingerNegCount = parseInt(gingerQty.textContent);
+    gingerNegCount = gingerNegCount - 1;
+    if (gingerNegCount === -1) {
+        gingerQty.textContent === 0
+    } else {
+        gingerQty.textContent = gingerNegCount;
+    }
+
+    totalQtyCount = parseInt(totalQty.textContent);
+    totalQtyCount = totalQtyCount - 1;
+    if (totalQtyCount === -1) {
+        totalQty.textContent === 0
+    } else {
+        totalQty.textContent = totalQtyCount;
+    }
+});
+
+
+
+//CHOC CHIP
+let plusChocBtn = document.querySelector("#add-cc")
+let minusChocBtn = document.querySelector("#minus-cc")
+let chocQty = document.querySelector("#qty-cc")
+
+plusChocBtn.addEventListener('click', (e) => {
+    chocPosCount = parseInt(chocQty.textContent);
+    chocPosCount = chocPosCount + 1;
+    chocQty.textContent = chocPosCount;
+
+    totalQtyCount = parseInt(totalQty.textContent);
+    totalQtyCount = totalQtyCount + 1;
+    totalQty.textContent = totalQtyCount;
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+minusChocBtn.addEventListener('click', (e) => {
+    chocNegCount = parseInt(chocQty.textContent);
+    chocNegCount = chocNegCount - 1;
+    if (chocNegCount === -1) {
+        chocQty.textContent === 0
+    } else {
+        chocQty.textContent = chocNegCount;
+    }
+
+    totalQtyCount = parseInt(totalQty.textContent);
+    totalQtyCount = totalQtyCount - 1;
+    if (totalQtyCount === -1) {
+        totalQty.textContent === 0
+    } else {
+        totalQty.textContent = totalQtyCount;
+    }
+})
+
+//SUGAR SPRINKLE
+let plusSugarBtn = document.querySelector("#add-sugar")
+let minusSugarBtn = document.querySelector("#minus-sugar")
+let sugarQty = document.querySelector("#qty-sugar")
+
+plusSugarBtn.addEventListener('click', (e) => {
+    sugarPosCount = parseInt(sugarQty.textContent);
+    sugarPosCount = sugarPosCount + 1;
+    sugarQty.textContent = sugarPosCount;
+
+    totalQtyCount = parseInt(totalQty.textContent);
+    totalQtyCount = totalQtyCount + 1;
+    totalQty.textContent = totalQtyCount;
+})
+
+minusSugarBtn.addEventListener('click', (e) => {
+    sugarNegCount = parseInt(sugarQty.textContent);
+    sugarNegCount = sugarNegCount - 1;
+    if (sugarNegCount === -1) {
+        sugarQty.textContent === 0
+    } else {
+        sugarQty.textContent = sugarNegCount
+    };
+    totalQty
+
+    totalQtyCount = parseInt(totalQty.textContent);
+    totalQtyCount = totalQtyCount - 1;
+    if (totalQtyCount === -1) {
+        totalQty.textContent === 0
+    } else {
+        totalQty.textContent = totalQtyCount;
+    }
+})
